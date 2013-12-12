510240cb51e3ff0e651cc7866f2524f7b878f618
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-12-12 15:55:53 +0200

    Added some conf things

--- main.js
+++ main.js
@@ -5,11 +5,31 @@ function ulysses(opts) {
     if(!opts) {
         opts = {};
     }
+    if(!opts.exec) {
+        throw new Error("Worker must be specified");
+    }
+    this.exec = opts.exec;
+    this.workers = {};
+    this.args = opts.args || [];
+    this.silent = opts.silent || true;
     this.numOfWorkers = opts.workerNum || os.cpus().length;
+    this.env = opts.env || {};
+    cluster.setupMaster({
+        exec: this.exec,
+        args: this.args,
+        silent: this.silent
+    });
 }
 
-ulysses.prototype.start = function() {
+ulysses.prototype.start = function(callback) {
+    var self = this;
+    process.nextTick(function() {
+        i = 0;
+        while(i < self.numOfWorkers) {
 
+            i++;
+        }
+    });
 }
 
 ulysses.prototype.stop = function() {
