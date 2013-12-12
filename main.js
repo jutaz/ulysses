f8e2d7a7a70b29be1d6e635f2e1984a50aa5cca0
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-12-12 15:44:00 +0200

    Add options

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var cluster = require('cluster');
+var os = require('os');
 
 function ulysses(opts) {
     if(!opts) {
         opts = {};
     }
+    this.numOfWorkers = opts.workerNum || os.cpus().length;
 }
 
 ulysses.prototype.start = function() {
@@ -11,15 +13,15 @@ ulysses.prototype.start = function() {
 }
 
 ulysses.prototype.stop = function() {
-    
+
 }
 
 ulysses.prototype.restart = function() {
-    
+
 }
 
 ulysses.prototype.reload = function() {
-    
+
 }
 
 
